bb1dfe87def589d509b23344aac41fc164b89105
ᔕᖺᘎᕊ <shu8@users.noreply.github.com>
2018-12-27 10:42:41 +0000

    Clean up code

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
 /*jshint esversion:6*/
-var beautify = require('js-beautify').js_beautify;
+const prettier = require("prettier");
+module.exports = function (app) {
+  app.on('pull_request', async function (context) {
+    const config = prettier.resolveConfig.sync('prettifierV2.yml', {
+      'config': '.github/prettifierV2.yml'
+    }) || {};
 
-module.exports = function(app) {
-    app.on('pull_request', async function(context) {
-        const config = await context.config('prettifier.yml');
+    if (context.payload.sender.type === 'Bot') return;
 
-        if (context.payload.sender.type === 'Bot') return;
+    const owner = context.payload.pull_request.head.user.login;
+    const repo = context.payload.pull_request.head.repo.name;
+    const number = context.payload.number;
 
-        let owner = context.payload.pull_request.head.user.login,
-            repo = context.payload.pull_request.head.repo.name,
-            number = context.payload.number;
-
-        context.github.pullRequests.getFiles({
-            owner: owner,
-            repo: repo,
-            number: number,
-            per_page: 100
-        }).then(function(files) {
-            files = files.data;
-            for (let f = 0; f < files.length; f++) {
-                let filenameSplit = files[f].filename.split('.');
-
-                if (filenameSplit[filenameSplit.length - 1] !== 'js') return; //not a JS file
-
-                context.github.repos.getContent({
-                    owner: owner,
-                    repo: repo,
-                    path: files[f].filename,
-                    ref: context.payload.pull_request.head.ref
-                }).then(function(file) {
-                    let fileContentsBase64 = file.data.content.trim(),
-                        fileContentsAscii = new Buffer(fileContentsBase64, 'base64').toString('ascii'),
-                        prettyFileContentsAscii = config ? beautify(fileContentsAscii, config.js) : beautify(fileContentsAscii),
-                        prettyFileContentsBase64 = new Buffer(prettyFileContentsAscii).toString('base64');
-
-                    if (fileContentsAscii === prettyFileContentsAscii) return; //if already pretty, no update needed
-
-                    context.github.repos.updateFile({
-                        owner: owner,
-                        repo: repo,
-                        path: files[f].filename,
-                        message: 'Prettify code',
-                        content: prettyFileContentsBase64,
-                        sha: files[f].sha,
-                        branch: context.payload.pull_request.head.ref
-                    });
-                });
-            }
-        });
+    const files = await context.github.pullRequests.getFiles({
+      owner: owner,
+      repo: repo,
+      number: number,
+      per_page: 100
     });
+    if (!files) return;
+    console.log(files);
+
+    files = files.data;
+    for (let f = 0; f < files.length; f++) {
+      const file = await context.github.repos.getContent({
+        owner: owner,
+        repo: repo,
+        path: files[f].filename,
+        ref: context.payload.pull_request.head.ref
+      });
+      if (!file) continue;
+
+      const fileContentsBase64 = file.data.content.trim();
+      const fileContentsAscii = new Buffer(fileContentsBase64, 'base64').toString('ascii');
+      const prettyFileContentsAscii = prettier.format(fileContentsAscii, config);
+      const prettyFileContentsBase64 = new Buffer(prettyFileContentsAscii).toString('base64');
+
+      // If already pretty, no update needed
+      if (fileContentsAscii === prettyFileContentsAscii) continue;
+
+      context.github.repos.updateFile({
+        owner: owner,
+        repo: repo,
+        path: files[f].filename,
+        message: '[ProbotPrettifier] Prettify code',
+        content: prettyFileContentsBase64,
+        sha: files[f].sha,
+        branch: context.payload.pull_request.head.ref
+      });
+    }
+  });
 };
